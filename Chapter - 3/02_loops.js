785776b4209aee188c58b3248b264c750a1aa7dc
Tushar Nebhnani <nebhnani.tushar8@gmail.com>
2025-07-31 11:07:57 +0530

    Adding loops

---
+++ Chapter - 3/02_loops.js
@@ -0,0 +1,24 @@
+// if checks for one time while loops checks again n again 
+// GuideLines for loops:
+// keyword -> while, do-while, for, for-of, for-each, for-in
+// termination CSSConditionRule, loop can be infinite
+// execution statements (body of loop)
+
+// challenge 1 
+let i = 1, sum = 0
+while (i <= 5) {
+    sum = i + sum
+    i++
+}
+console.log(sum);
+
+// challenge 2
+let j = 5
+let arr = []
+while(j > 0) {
+    arr.push(j)
+    j--
+} 
+console.log(arr);
+
+// challenge 3 
